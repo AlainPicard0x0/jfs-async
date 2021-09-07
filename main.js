b1771ff7fbb67b56a9962e043f357cc131fce650
Mark Warner <4764698+thrivethru@users.noreply.github.com>
2021-09-07 12:11:19 -0500

    Implement api call and update rates

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // Make sure the DOM Content is loaded before running
-if(document.readyState == "loading") {
-    document.addEventListener("DOMContentLoaded", main);
-}
-else {
-    main();
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", main);
+} else {
+  main();
 }
 
 // Get the p elements from index.html
@@ -14,32 +13,44 @@ const eurRateDisplay = document.getElementById("rate-display-eur");
 // Get the "Get Rate" button from index.html
 const getRateButton = document.getElementById("get-rate");
 
-function main() {    
-    // Add event listener to the "Get Rate" button 
-    // and run the "getRate" function when button is clicked
-    getRateButton.addEventListener("click", getRate)
+function main() {
+  // Add event listener to the "Get Rate" button
+  // and run the "getRate" function when button is clicked
+  getRateButton.addEventListener("click", getRate);
 }
 
 function getRate() {
-    // **** CoinDesk API **** Bitcoin price index ****
-    const api = "https://api.coindesk.com/v1/bpi/currentprice.json";
-    /*     
-    *    You can use fetch to access the above api 
-    *    Example of using fetch                           
-    *    fetch('http://example.com/movies.json')    
-    *    .then(response => response.json())         
-    *    .then(data => console.log(data));          
-    *
-    *    use fetch to access api and return response in JSON format
-    *
-    *    Once you've received your JSON, assign the values to the innerText 
-    *    of your p elements.
-    *
-    *    Example:
-    *    let val = 50;
-    *    usdRateDisplay.innerText = val;    *
-    *
-    *    *** Write code below here *** */
+  // **** CoinDesk API **** Bitcoin price index ****
+  const api = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  /*
+   *    You can use fetch to access the above api
+   *    Example of using fetch
+   *    fetch('http://example.com/movies.json')
+   *    .then(response => response.json())
+   *    .then(data => console.log(data));
+   *
+   *    use fetch to access api and return response in JSON format
+   *
+   *    Once you've received your JSON, assign the values to the innerText
+   *    of your p elements.
+   *
+   *    Example:
+   *    let val = 50;
+   *    usdRateDisplay.innerText = val;    *
+   *
+   *    *** Write code below here *** */
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => data.bpi)
+    .then((rates) => updateRates(rates));
+}
 
-    
+function updateRates(rates) {
+  let usdRate = rates.USD.rate;
+  let gbpRate = rates.GBP.rate;
+  let eurRate = rates.EUR.rate;
+
+  usdRateDisplay.innerText = usdRate;
+  gbpRateDisplay.innerText = gbpRate;
+  eurRateDisplay.innerText = eurRate;
 }
